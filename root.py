4fd20a27e7806587e7290da3e1c2af156428794a
Mohammad Faisal <mohammad.faisal@strativ.se>
2023-02-09 11:12:09 +0600

    wip

--- root.py
+++ root.py
@@ -1,7 +1,15 @@
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 main_app = FastAPI()
+
+
+# define model
+class Product(BaseModel):
+    name : str
+    price : float
+    is_active : Union[bool, None] = None
 
 @main_app.get("/")
 async def root_method():
@@ -26,3 +34,8 @@ async def read_item_list():
 async def read_item_detail(item_id: int, search: Union[str, None] = None):
     item = item_list.get(item_id)
     return {"item_id": item_id, "item":item}
+
+
+@main_app.put("/products/{product_id}")
+async def update_product(product_id: int, product: Product):
+    return {"product_name": product.name, "product_id": product_id, "price":product.price}
